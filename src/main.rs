bf76e4af0ba6799de71ec9d1fc3dd5f8cdc524af
Helder Martins <heldermartins89@gmail.com>
2019-02-17 21:36:18 +0000

    Support to build internal movies structure

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,26 @@ extern crate reqwest;
 
 extern crate futures;
 
+extern crate tokio;
 
+extern crate serde_json;
+
+extern crate serde;
+
+extern crate failure;
+
+mod models;
 mod routes;
+
+use models::movies;
+use routes::movies::BASE_URL;
+use routes::movies::SERVICE_REQUEST_PATH;
+
+use chrono::Utc;
+use failure::Error;
+use serde::Deserialize;
+use serde_json::Value;
+use std::collections::HashMap;
 
 #[get("/")]
 fn index() -> &'static str {
@@ -18,7 +36,67 @@ fn index() -> &'static str {
 }
 
 fn main() {
-    rocket::ignite()
-        .mount("/", routes![index, routes::movies::list_movies_in_display,])
-        .launch();
+    let tmp = load_data();
+    match tmp {
+        Ok(res) => {
+            let v: serde_json::Result<Value> = serde_json::from_str(res.as_str());
+            match v {
+                Ok(wat) => println!("fine and dandy"),
+                Err(error) => println!("mehhh..."),
+            }
+        }
+
+        Err(some) => println!("{:#?}", some),
+    }
+
+    /*    rocket::ignite()
+    .mount("/", routes![index, routes::movies::list_movies_in_display,])
+    .launch();*/
+}
+
+#[derive(Deserialize)]
+struct ServiceMovies {
+    d: Vec<ServiceMovie>,
+}
+
+#[derive(Deserialize)]
+struct ServiceMovie {
+    #[serde(alias = "Name")]
+    name: String,
+    #[serde(alias = "Link")]
+    link: String,
+    #[serde(alias = "PremiereDate")]
+    premiere_date: String,
+    #[serde(alias = "ImageUrl")]
+    image_url: String,
+}
+
+fn load_data() -> Result<String, Error> {
+    let json_text = make_external_request()?;
+    let movies: ServiceMovies = serde_json::from_str(json_text.as_str())?;
+
+    Ok(json_text)
+}
+
+fn make_external_request() -> Result<String, Error> {
+    let client = reqwest::Client::new();
+    let res = client
+        .post(format!("{}{}", BASE_URL, SERVICE_REQUEST_PATH).as_str())
+        .header(
+            reqwest::header::CONTENT_TYPE,
+            reqwest::header::HeaderValue::from_static("application/json"),
+        )
+        .send()?
+        .text()?;
+    Ok(res)
+}
+
+fn from_service_movies(service_movies: ServiceMovies) -> movies::MovieList {
+    let movies = service_movies
+        .d
+        .iter()
+        .map(|sm| movies::Movie::new(sm.name.clone(), sm.link.clone(), HashMap::new(), Utc::now()))
+        .collect();
+
+    movies::MovieList::new(movies)
 }
--- src/models/movies.rs
+++ src/models/movies.rs
@@ -1,3 +1,10 @@
+use chrono::DateTime;
+use chrono::Utc;
+use std::cmp;
+use std::collections::HashMap;
+
+type MovieProperty = HashMap<String, String>;
+
 pub struct Movie {
     name: String,
     url: String,
@@ -5,4 +12,43 @@ pub struct Movie {
     premiere_date: DateTime<Utc>,
 }
 
-type MovieProperty = HashMap<String, String>;
+pub struct MovieList {
+    movies: Vec<Movie>,
+    page_size: usize,
+}
+
+impl MovieList {
+    pub fn new(args: Vec<Movie>) -> MovieList {
+        MovieList {
+            movies: args,
+            page_size: 5,
+        }
+    }
+
+    pub fn getPage(&self, page_number: usize) -> &[Movie] {
+        let offset = (page_number - 1) * self.page_size;
+        let tail = cmp::min(self.movies.len() - 1, offset + (self.page_size - 1));
+
+        &self.movies[offset..tail]
+    }
+
+    pub fn totalPages(&self) -> u8 {
+        ((self.movies.len() as f64) / (self.page_size as f64)).ceil() as u8
+    }
+}
+
+impl Movie {
+    pub fn new(
+        name: String,
+        url: String,
+        properties: MovieProperty,
+        premiere_date: DateTime<Utc>,
+    ) -> Movie {
+        Movie {
+            name,
+            url,
+            properties,
+            premiere_date,
+        }
+    }
+}
